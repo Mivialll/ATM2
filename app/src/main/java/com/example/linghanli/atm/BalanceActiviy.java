d5e2ec892eb8af7724c3503c77d4e76c23987daf
Yiping Huang <31947209+YH-YipingHuang@users.noreply.github.com>
2017-11-29 21:36:08 -0400

    Add files via upload

---
+++ app/src/main/java/com/example/linghanli/atm/BalanceActiviy.java
@@ -0,0 +1,18 @@
+package com.example.huang.atm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DataSnapshot;
+
+public class BalanceActiviy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_balance_activiy);
+    }
+
+//public void onChildAdded(DataSnapshot  dataSnapshot )
+
+}
