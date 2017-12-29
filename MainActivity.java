615c0ce256674d6ff6cdb73af10ea0af14fa9729
Mivia(Linghan) Li <32752716+Mivialll@users.noreply.github.com>
2017-12-29 16:29:52 -0500

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,31 @@
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 = (Button) findViewById(R.id.mainbt1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ATMActivity.class));
+            }
+        });
+    }
+
+}
